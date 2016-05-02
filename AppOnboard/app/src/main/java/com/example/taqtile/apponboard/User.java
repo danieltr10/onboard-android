150a1daea9c0f5254c0ea3efda2252326f6901ab
danieltr10 <danieltr10@github.com>
2016-05-02 18:22:45 -0300

    Camel Case e Private nos atributos da classe User

--- AppOnboard/app/src/main/java/com/example/taqtile/apponboard/User.java
+++ AppOnboard/app/src/main/java/com/example/taqtile/apponboard/User.java
@@ -4,10 +4,10 @@ package com.example.taqtile.apponboard;
  * Created by taqtile on 5/2/16.
  */
 public class User {
-    int id;
-    String first_name;
-    String last_name;
-    String avatar;
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String avatar;
 
     public int getId() {
         return id;
@@ -17,20 +17,20 @@ public class User {
         this.id = id;
     }
 
-    public String getFirst_name() {
-        return first_name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public String getLast_name() {
-        return last_name;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getAvatar() {
@@ -40,5 +40,4 @@ public class User {
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
-
 }
