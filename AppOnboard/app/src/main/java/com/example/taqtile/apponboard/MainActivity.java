0761ff047a3a7c802552cf768e893e29713ea01b
danieltr10 <danieltr10@github.com>
2016-05-03 16:47:12 -0300

    Convencoes Java e melhora na leitura do codigo

--- AppOnboard/app/src/main/java/com/example/taqtile/apponboard/MainActivity.java
+++ AppOnboard/app/src/main/java/com/example/taqtile/apponboard/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private List<User> usersList = new ArrayList<>();
-    private RecyclerView recyclerView;
+    private List<User> mUsersList = new ArrayList<>();
+    private RecyclerView mRecyclerView;
     private UsersAdapter mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +26,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "Mensagem de Log");
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-
-        mAdapter = new UsersAdapter(usersList);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
-        recyclerView.setAdapter(mAdapter);
+        configurarRecyclerView();
 
         prepareUserData();
+
+    }
+
+    private void configurarRecyclerView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        mAdapter = new UsersAdapter(mUsersList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.addItemDecoration(new SimpleDividerItemDecoration(this));
+        mRecyclerView.setAdapter(mAdapter);
 
     }
 
@@ -42,79 +49,79 @@ public class MainActivity extends AppCompatActivity {
         user.setFirstName("Daniel");
         user.setLastName("Rizzuto");
         user.setId(0);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Daniel");
         user.setLastName("Nakasato");
         user.setId(1);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Roberto");
         user.setLastName("Rizzuto");
         user.setId(2);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Laura");
         user.setLastName("Regina");
         user.setId(3);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Rafael");
         user.setLastName("Trigo");
         user.setId(4);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Daniel");
         user.setLastName("Lobato");
         user.setId(5);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Roberta");
         user.setLastName("Truppa");
         user.setId(6);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Arnaldo");
         user.setLastName("Coelho");
         user.setId(7);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Bruno");
         user.setLastName("Fagundes");
         user.setId(8);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Monteiro");
         user.setLastName("Lobato");
         user.setId(9);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Zé");
         user.setLastName("Carioca");
         user.setId(10);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Amanda");
         user.setLastName("Trimer");
         user.setId(11);
-        usersList.add(user);
+        mUsersList.add(user);
 
         user = new User();
         user.setFirstName("Paloma");
         user.setLastName("Oliveira");
         user.setId(12);
-        usersList.add(user);
+        mUsersList.add(user);
 
         mAdapter.notifyDataSetChanged();
 
